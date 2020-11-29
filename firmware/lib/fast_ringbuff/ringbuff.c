90f80482549a6233bf59cf926c8b82e264d54859
solich919 <kuba919@gmail.com>
2020-11-29 18:39:04 +0100

    updated lib, added string

--- firmware/lib/fast_ringbuff/ringbuff.c
+++ firmware/lib/fast_ringbuff/ringbuff.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 struct ring_buffer
 {
@@ -6,20 +8,20 @@ struct ring_buffer
     uint8_t read_pointer;
     uint8_t write_pointer;
     uint8_t mask;
-    int8_t counter;
+    uint8_t counter;
 };
 
-bool buffer_is_full(struct ring_buffer buffer_name)
+bool buffer_is_full(struct ring_buffer *buffer_name)
 {
-    if(buffer_name.counter > buffer_name.mask)
+    if(buffer_name->counter > buffer_name->mask)
         return 1;
     else
         return 0;
 }
 
-bool buffer_is_empty(struct ring_buffer buffer_name)
+bool buffer_is_empty(struct ring_buffer *buffer_name)
 {
-    if(buffer_name.counter == 0)
+    if(buffer_name->counter == 0)
         return 1;
     else
         return 0;
@@ -45,7 +47,7 @@ void buffer_write_byte(struct ring_buffer *buffer_name, uint8_t byte)
 void buffer_init(struct ring_buffer *buffer_name, uint8_t n_size)
 {
     buffer_name->mask = (1<<n_size)-1;
-    buffer_name->buffer = malloc(size);
+    buffer_name->buffer = malloc(1<<n_size);
     buffer_name->write_pointer = 0;
     buffer_name->read_pointer = 0;
     buffer_name->counter = 0;
@@ -55,4 +57,10 @@ void buffer_reset(struct ring_buffer *buffer_name)
 {
     buffer_name->read_pointer = buffer_name->write_pointer;
     buffer_name->counter = 0;
+}
+
+void buffer_string(struct ring_buffer *buffer_name, char *string)
+{
+    for(uint8_t i = 0; string[i] != 0x00; i++)
+        buffer_write_byte(buffer_name, string[i]);
 }
--- firmware/lib/fast_ringbuff/ringbuff.h
+++ firmware/lib/fast_ringbuff/ringbuff.h
@@ -1,8 +1,20 @@
 #pragma once
-struct ring_buffer;
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+struct ring_buffer
+{
+    uint8_t *buffer;
+    uint8_t read_pointer;
+    uint8_t write_pointer;
+    uint8_t mask;
+    int8_t counter;
+};
 void buffer_init(struct ring_buffer *buffer_name, uint8_t n_size);
 void buffer_reset(struct ring_buffer *buffer_name);
 uint8_t buffer_read_byte(struct ring_buffer *buffer_name);
 void buffer_write_byte(struct ring_buffer *buffer_name, uint8_t byte);
-bool buffer_is_full(struct ring_buffer buffer_name);
-bool buffer_is_empty(struct ring_buffer buffer_name);
+bool buffer_is_full(struct ring_buffer *buffer_name);
+bool buffer_is_empty(struct ring_buffer *buffer_name);
+void buffer_string(struct ring_buffer *buffer_name, char *string);
