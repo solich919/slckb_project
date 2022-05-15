ed809277a7542a4848cbf374cf7b73044b1f6bb3
SLC <kuba919@gmail.com>
2022-05-15 23:13:13 +0200

    Delete firmware directory

--- firmware/lib/simple_uart/simple_uart.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <avr/io.h>
-#include "../fast_ringbuff/ringbuff.h"
-
-void UART_Init(uint16_t baud);
-void UART_Transmit(uint8_t data);
-void UART_Dump(struct ring_buffer* buffer_name);
